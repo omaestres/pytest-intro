f487d8ea531e4e689b633010df428be2a68ebe7e
unknown <omaestre@TEMP-C-OscarM.fsoft.fpt.vn>
2021-11-16 17:14:19 -0500

    Updating .gitignore

--- tests/test_math.py
+++ tests/test_math.py
@@ -5,4 +5,10 @@ Example unit tests for math operations
 # A basic test function
 def test_one_plus_one():
     assert 1 + 1 == 2
+
+def test_failure():
+    a = 1
+    b = 2
+    c = 0
+    assert a + b == c
     
